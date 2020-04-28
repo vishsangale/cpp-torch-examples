04c9c70536455ae6e51fc8311e4aece58a99782d
Vishwanath Sangale <vishsangale@gmail.com>
2020-04-27 22:11:46 -0700

    Simple cuda related utils

---
+++ cuda_utils/example.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#include <torch/torch.h>
+
+int main ()
+{
+    torch::manual_seed(13);
+
+    torch::Device device = torch::kCPU;
+    if (torch::cuda::is_available())
+    {
+        std::cout << "CUDA is available, using  GPU." << std::endl;
+        device = torch::kCUDA;
+    }
+    else
+    {
+        std::cout << "CUDA not avaialble, using CPU." << std::endl;
+    }
+    return 0;
+}
